d32af28f29ac65b56b64beb06db239c26cf78148
Rodolfo M <rodolfomarin2020@gmail.com>
2022-11-28 23:52:01 -0400

    ult cambioss

--- src/proyecto2/Proyecto2.java
+++ src/proyecto2/Proyecto2.java
@@ -17,43 +17,7 @@ public class Proyecto2 {
     public static void main(String[] args) {
         InterfazPrincipal window = new InterfazPrincipal();
         window.setVisible(true);
-        
-
-//        ArbolAB arbol = new ArbolAB();
-//        arbol.Insertar("P", null, null);
-//        arbol.Insertar("A", "P","left");
-//        grafico = arbol.CrearGrafica_arbol();
-//        arbol.CrearNodes(grafico, arbol.getRoot());
-//        arbol.CrearEdges(grafico, arbol.getRoot());
-        
-
-//        String[] info = {"(3*x^2+2*x*y^2+8*y^3)/(8*x^3+3*x^2*y^2)"};
-//        
-//        for (int i = 0; i < info.length; i++) {
-//            System.out.println(info[i]);
-//            String[] infoBuena = info[i].split("");
-//            for (int j = 0; j < infoBuena.length; j++) {
-//                System.out.println(infoBuena[j]);
-//                
-//            }
-////            
-//        }
-        
-//        System.out.println(info.split(""));
-        
-
-        
-//        System.out.println(info.length());
-//        System.out.println(cadena_split.length());
-        
-//        System.out.println(letras_cadena.length);
-        
-//        for (int i = 0; i < letras_cadena.length; i++) {
-//            System.out.println(letras_cadena[i]);
-//        }
-//            String[] cadenaBuena = cadena_split[i].split("");
-            
-        
+                
 
     }
 
